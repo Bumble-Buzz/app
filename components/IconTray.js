56774b2dd57e41dca1dbaf8c15a4dfb755a03bc3
arizvi125 <aaarizvi125@gmail.com>
2022-03-06 17:53:36 -0500

    Added refresh metadata button in icon tray

--- components/IconTray.js
+++ components/IconTray.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { MenuIcon } from '@heroicons/react/solid';
 import DropDown from './navbar/DropDown';
+import { RefreshIcon } from '@heroicons/react/solid';
 
 
 export default function IconTray({ items }) {
@@ -21,6 +22,9 @@ export default function IconTray({ items }) {
 
   return (
     <div className='grid grid-rows-1 grid-flow-col w-fit divide-x border rounded-lg shadow-lg bg-gray-50 items-center text-center'>
+      <div className='relative pl-1 mr-3 h-5 w-5 transform transition duration-500 hover:scale-105 cursor-pointer'>
+        <RefreshIcon className="w-5 h-5 mr-2" aria-hidden="true" />
+      </div>
       {items && items.map((item, index) => {
         return (
           <div className='px-4 my-2 relative h-5 w-5 transform transition duration-500 hover:scale-105 cursor-pointer' key={index}>
