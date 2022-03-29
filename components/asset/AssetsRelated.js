ff87c06984f1bf45114b6f6ef775655ce6126e4a
arizvi125 <aaarizvi125@gmail.com>
2022-03-29 08:57:34 -0400

    Asset page, related assets: updated gap and card sizes

--- components/asset/AssetsRelated.js
+++ components/asset/AssetsRelated.js
@@ -37,13 +37,13 @@ export default function AssetsRelated({ initialData, classes }) {
     <>
       <div className={`flex flex-row overflow-x-auto w-full ${classes}`}>
         <div className='w-max'>
-          <div className='px-2 py-2 flex flex-row w-full gap-10'>
+          <div className='px-2 py-2 flex flex-row w-full gap-4'>
 
             {filteredRelatedAssets.map((asset, index) => {
               return (
                 <div
                   key={index}
-                  className='w-full grow w-36 xsm:w-40 sm:w-60 max-w-xs border rounded-lg overflow-hidden shadow-lg transform transition duration-500 hover:scale-105 cursor-pointer'
+                  className='w-full grow w-40 sm:w-56 border rounded-lg overflow-hidden shadow-lg transform transition duration-500 hover:scale-105 cursor-pointer'
                   onClick={() => ROUTER.push(`/asset/${asset.contractAddress}/${asset.tokenId}`)}
                 >
                   <NftCard
