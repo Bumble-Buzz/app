88ccfb6787e885050e6cae58142f4b1c062f3f74
arizvi125 <aaarizvi125@gmail.com>
2022-04-21 11:34:10 -0400

    ENUM: Updated Network names

--- enum/Networks.js
+++ enum/Networks.js
@@ -1,33 +1,33 @@
 export default {
   local: {
-    mainnet: { name: 'local', id: 1337, currency: 'ETH' }
+    mainnet: { name: 'Local', id: 1337, currency: 'ETH' }
   },
   ethereum: {
-    mainnet: { name: 'ethereum', id: 1, currency: 'ETH' },
-    ropsten: { name: 'ropsten', id: 3, currency: 'ETH' },
-    rinkeby: { name: 'rinkeby', id: 4, currency: 'ETH' },
-    goerli: { name: 'goerli', id: 5, currency: 'ETH' },
-    kovan: { name: 'kovan', id: 42, currency: 'ETH' }
+    mainnet: { name: 'Ethereum', id: 1, currency: 'ETH' },
+    ropsten: { name: 'Ropsten', id: 3, currency: 'ETH' },
+    rinkeby: { name: 'Rinkeby', id: 4, currency: 'ETH' },
+    goerli: { name: 'Goerli', id: 5, currency: 'ETH' },
+    kovan: { name: 'Kovan', id: 42, currency: 'ETH' }
   },
   binance: {
-    mainnet: { name: 'binance', id: 56, currency: 'BNB' },
-    testnet: { name: 'testnet', id: 97, currency: 'BNB' }
+    mainnet: { name: 'Binance', id: 56, currency: 'BNB' },
+    testnet: { name: 'Testnet', id: 97, currency: 'BNB' }
   },
   avalanche: {
-    mainnet: { name: 'avalanche', id: 43114, currency: 'AVAX' },
-    testnet: { name: 'testnet', id: 43113, currency: 'AVAX' }
+    mainnet: { name: 'Avalanche', id: 43114, currency: 'AVAX' },
+    testnet: { name: 'Testnet', id: 43113, currency: 'AVAX' }
   },
   aurora: {
-    mainnet: { name: 'aurora', id: 1313161554, currency: 'ETH' },
-    testnet: { name: 'testnet', id: 1313161555, currency: 'ETH' },
-    betanet: { name: 'betanet', id: 1313161556, currency: 'ETH' }
+    mainnet: { name: 'Aurora', id: 1313161554, currency: 'ETH' },
+    testnet: { name: 'Testnet', id: 1313161555, currency: 'ETH' },
+    betanet: { name: 'Betanet', id: 1313161556, currency: 'ETH' }
   },
   fantom: {
-    mainnet: { name: 'fantom', id: 250, currency: 'FTM' },
-    testnet: { name: 'testnet', id: 4002, currency: 'FTM' }
+    mainnet: { name: 'Fantom', id: 250, currency: 'FTM' },
+    testnet: { name: 'Testnet', id: 4002, currency: 'FTM' }
   },
   polygon: {
-    mainnet: { name: 'polygon', id: 137, currency: 'MATIC' },
+    mainnet: { name: 'Polygon', id: 137, currency: 'MATIC' },
     mumbai: { name: 'mumbai', id: 80001, currency: 'MATIC' }
   }
 };
