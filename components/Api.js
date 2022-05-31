de2d683bc2bbc7b7b2511b7b6bb940b15d1191bd
arizvi125 <aaarizvi125@gmail.com>
2022-05-31 10:43:54 -0400

    Removed/commented direct DB APIs. Deleted old db methods in asset/create page.

--- components/Api.js
+++ components/Api.js
@@ -122,25 +122,25 @@ module.exports = {
       ownerincentiveaccess: (id,payload) => API.post(`collection/update/${id}/ownerincentiveaccess`, payload)
     }
   },
-  db: {
-    table: {
-      list: payload => API.put(`db/table/list`, payload),
-      create: payload => API.put(`db/table/create`, payload),
-      status: payload => API.put(`db/table/status`, payload),
-      delete: payload => API.put(`db/table/delete`, payload),
-      scan: payload => API.put(`db/table/scan`, payload)
-    },
-    item: {
-      get: payload => API.put(`db/item/get`, payload),
-      getBatch: payload => API.put(`db/item/getBatch`, payload),
-      put: payload => API.put(`db/item/put`, payload),
-      putBatch: payload => API.put(`db/item/putBatch`, payload),
-      update: payload => API.put(`db/item/update`, payload),
-      delete: payload => API.put(`db/item/delete`, payload),
-      deleteBatch: payload => API.put(`db/item/deleteBatch`, payload),
-      scan: payload => API.put(`db/item/scan`, payload)
-    },
-  },
+  // db: {
+  //   table: {
+  //     list: payload => API.put(`db/table/list`, payload),
+  //     create: payload => API.put(`db/table/create`, payload),
+  //     status: payload => API.put(`db/table/status`, payload),
+  //     delete: payload => API.put(`db/table/delete`, payload),
+  //     scan: payload => API.put(`db/table/scan`, payload)
+  //   },
+  //   item: {
+  //     get: payload => API.put(`db/item/get`, payload),
+  //     getBatch: payload => API.put(`db/item/getBatch`, payload),
+  //     put: payload => API.put(`db/item/put`, payload),
+  //     putBatch: payload => API.put(`db/item/putBatch`, payload),
+  //     update: payload => API.put(`db/item/update`, payload),
+  //     delete: payload => API.put(`db/item/delete`, payload),
+  //     deleteBatch: payload => API.put(`db/item/deleteBatch`, payload),
+  //     scan: payload => API.put(`db/item/scan`, payload)
+  //   },
+  // },
   backend: {
     user: {
       id: (id) => BACKEND_API.get(`user/${id}`)
